c843fe40bb9d60ea1473ad5a9a55c66cc85f1ae0
LegalDesigner <hafsohomotosho@gmail.com>
2023-07-16 16:46:08 -0500

    mobile responsiveness

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.querySelector(".hamburger");
+    const navLinks = document.querySelector(".navbar-links");
+  
+    hamburger.addEventListener("click", () => {
+      hamburger.classList.toggle("active");
+      navLinks.classList.toggle("active");
+    });
+  });
+  
